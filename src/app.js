9e059f87cb1386bcc9df7aa4a12d67eeb0965426
ManideepArruri <manideeparruri@gmail.com>
2025-07-06 16:37:31 +0530

    exploring http methods and routes

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Arruri", lastName: "Manideep" });
+});
+
+app.post("/user", (req, res) => {
+  console.log("Save data to DB");
+  res.send("Data saved in DB");
+});
+
 app.use("/home", (req, res) => {
   res.send("Home Page");
 });
 
+app.use("/", (req, res) => {
+  res.send("Overrides");
+});
 app.listen("7777", (req, res) => {
   console.log("Server Connected!");
 });
